c6a9476a310d34c10254b7b9cbc8946089f1a16c
Erkha <jmtatout@gmail.com>
2019-06-09 18:17:02 +0200

    added Bootstrap JS on Adminbase, tabs on show new and computed field for show entity

--- src/Form/ShowType.php
+++ src/Form/ShowType.php
@@ -7,23 +7,18 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class ContentType extends AbstractType
+class ShowType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
             ->add('title_fr')
-            ->add('contentType')
-            ->add('status')
+
             ->add('title_en')
             ->add('content_fr')
             ->add('content_en')
             ->add('country_fr')
             ->add('country_en')
-            ->add('mapadoLink')
-            ->add('edition')
-            ->add('logos')
-            ->add('picture')
         ;
     }
 
