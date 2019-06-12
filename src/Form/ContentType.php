623a6817c0b41f3b253ecfadc05086f89ed1ac72
Jean Daniel Boccara <jdboccara@gmail.com>
2019-06-12 09:09:53 +0200

    PR

--- src/Form/ContentType.php
+++ src/Form/ContentType.php
@@ -1,23 +1,23 @@
 <?php
 
-namespace App\Form;
+  namespace App\Form;
 
-use App\Entity\Content;
-use FOS\CKEditorBundle\Form\Type\CKEditorType;
-use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
+  use App\Entity\Content;
+  use FOS\CKEditorBundle\Form\Type\CKEditorType;
+  use Symfony\Component\Form\AbstractType;
+  use Symfony\Component\Form\FormBuilderInterface;
+  use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class ContentType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('title_fr')
-            ->add('content_fr', CKEditorType::class)
-            ->add('content_type')
-            ->add('title_en')
-            ->add('content_en', CKEditorType::class)
+        ->add('title_fr')
+        ->add('content_fr', CKEditorType::class)
+        ->add('content_type')
+        ->add('title_en')
+        ->add('content_en', CKEditorType::class)
 
         ;
     }
@@ -25,7 +25,7 @@ class ContentType extends AbstractType
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
-          'data_class' => Content::class,
+        'data_class' => Content::class,
         ]);
     }
 }
