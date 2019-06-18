f6a5859d3e40227a72e3bcc8811c464229ba4209
Erkha <jmtatout@gmail.com>
2019-06-18 15:57:32 +0200

    reviewed fixtures to basically work

--- src/DataFixtures/ShowContentFixtures.php
+++ src/DataFixtures/ShowContentFixtures.php
@@ -6,38 +6,19 @@ use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use App\Entity\Content;
-use Faker;
 
 class ShowContentFixtures extends Fixture implements DependentFixtureInterface
 {
     public function load(ObjectManager $manager)
     {
-        $faker = Faker\Factory::create();
-        for ($i=0; $i < 15; $i++) {
-            $show = new Content();
-            $show->setTitleFr($faker->words($nb = 3, $asText = true));
-            $show->setEdition($this->getReference('edition'));
-            $show->setContentType(Content::CONTENT_TYPE['show']);
-            $show->setComplete(false);
-            if (rand(0, 1)) { // in order to have some incomplete shows
-                $show->setContentFr($faker->paragraphs($nb = 3, $asText = true));
-                $show->setCountryFr($faker->country);
-                $show->setPicture($this->getReference('picture_'.rand(0, 9)));
-                $show->setComplete(true);
-            }
-            for ($j=0; $j < rand(0, 2); $j++) {
-                $show->addLogo($this->getReference('logo_'.rand(0, 9)));
-            }
-            $show->setTranslated(false);
-            if (rand(0, 1)) { // in order to have only some translated shows
-                $show->setContentEn($faker->paragraphs($nb = 3, $asText = true));
-                $show->setTitleEn($faker->words($nb = 3, $asText = true));
-                $show->setCountryEn($faker->country);
-                $show->setTranslated(true);
-            }
-            $manager->persist($show);
-            $this->addReference('show_'.$i, $show);
-        }
+        
+        $show = new Content;
+        $show   ->setTitleFr('Banos Rama')
+                ->setContentType(Content::CONTENT_TYPE['show'])
+                ->setComplete(true)
+                ->setTranslated(true);
+        $this->addReference('showBamos', $show);
+        $manager->persist($show);
         $manager->flush();
     }
 
