743248a849fd5cc6ffb4c36f5ce088ba4f376297
Mussuranae <mandonnetdev@outlook.com>
2019-06-25 10:12:24 +0200

    RAS

--- src/Entity/Session.php
+++ src/Entity/Session.php
@@ -2,6 +2,8 @@
 
 namespace App\Entity;
 
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -31,6 +33,7 @@ class Session
      * @ORM\JoinColumn(nullable=false)
      */
     private $content;
+
 
     public function getId(): ?int
     {
