eaf23d59cbf039ab69f7d39acf42057fffcaeddf
Mussuranae <mandonnetdev@outlook.com>
2019-07-02 16:06:49 +0200

    on récupère bien les spectacles sur les archives !

--- src/Controller/ArchivesController.php
+++ src/Controller/ArchivesController.php
@@ -2,17 +2,34 @@
 
 namespace App\Controller;
 
+use App\Repository\ContentRepository;
 use App\Repository\EditionRepository;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
+/**
+ * @Route("/archives")
+ */
 class ArchivesController extends AbstractController
 {
     /**
-     * @Route("/archives", name="archives")
+     * @Route("/", name="archives")
      */
     public function archives(EditionRepository $editionRepository)
     {
         return $this->render('UserTemplate/archives.html.twig', ['editions' => $editionRepository->findAll()]);
+    }
+
+    /**
+     * @Route("/{id}", name="list_content_edition_archive")
+     */
+    public function contentEditionArchives(ContentRepository $contentRepository, EditionRepository $editionRepository)
+    {
+        $edition = $editionRepository->findAll();
+        $content = $contentRepository->findby(['edition' => $edition]);
+        return $this->render(
+            'UserTemplate/editionArchives.html.twig',
+            ['contents' => $content]
+        );
     }
 }
